7d7e544e7905550fc882f05039e235201dbe2199
Chaosmann09 <56198093+chaosmann09@users.noreply.github.com>
2024-10-23 21:14:40 +0200

    Create IPO.java

---
+++ IPO.java
@@ -0,0 +1,12 @@
+public class IPO {
+
+    Backofen miele = new Backofen("miele", 150);
+
+    Affe gorilla = new Affe("Marcel", "Gorilla", 150);
+
+    public IPO() {
+        System.out.println(miele.toString());
+        System.out.println(gorilla.toString());
+    }
+
+}
